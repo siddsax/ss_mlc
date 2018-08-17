dad797566a093c0ee6e091a6fce23e57164994c9
Siddhartha Saxena <ss.siddharthasaxena@gmail.com>
2018-08-18 01:30:11 +0300

    added dataset, loss and out layer not changed

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ np.random.seed(1337)
 params = argparse.ArgumentParser(description='Process some integers.')
 params.add_argument('--ss', dest='ss', type=int, default=1, help='1 to do semi-super, 0 for not doing it')
 params.add_argument('--nrml', dest='normal', type=int, default=0, help='1 to do semi-super, 0 for not doing it')
+params.add_argument('--ds', dest='data_set', type=str, default="mnist", help='mnist; delicious;')
+
 params = params.parse_args()
 params.cuda = torch.cuda.is_available()
 print("CUDA: {}".format(params.cuda))
@@ -28,11 +30,11 @@ print("CUDA: {}".format(params.cuda))
 if __name__ == "__main__":
     params.best = 0.0
     params.n_labels = 10
-    params.labelled, params.unlabelled, params.validation = get_mnist(params,location="./", batch_size=100, labels_per_class=100)
+    params = get_dataset(params)
     params.alpha = 0.1 * len(params.unlabelled) / len(params.labelled)
     params.epochs = 251
     params.step = 0
-    model = DeepGenerativeModel([784, 10, 50, [600, 600]])
+    model = DeepGenerativeModel([params.xdim, params.n_labels, 50, [600, 600]])
     if params.cuda:
         model = model.cuda()
     # , sampler=sampler) #,beta=beta)
