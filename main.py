2fbcd46989c881af87669c481e6a15bb5f37ac0c
siddsax <ss.siddharthasaxena@gmail.com>
2018-08-20 16:43:13 +0300

    dataset part changed

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from torch.autograd import Variable
 from inference import SVI, DeterministicWarmup, ImportanceWeightedSampler
 from modelPass import modelTrPass, modelTePass
 import argparse
+from visualizer import Visualizer
 torch.manual_seed(1337)
 np.random.seed(1337)
 
@@ -21,18 +22,23 @@ np.random.seed(1337)
 params = argparse.ArgumentParser(description='Process some integers.')
 params.add_argument('--ss', dest='ss', type=int, default=1, help='1 to do semi-super, 0 for not doing it')
 params.add_argument('--nrml', dest='normal', type=int, default=0, help='1 to do semi-super, 0 for not doing it')
+params.add_argument('--ds', dest='data_set', type=str, default="mnist", help='mnist; delicious;')
+params.add_argument('--mn', dest='name', type=str, default="", help='mnist; delicious;')
+
 params = params.parse_args()
 params.cuda = torch.cuda.is_available()
 print("CUDA: {}".format(params.cuda))
 
 if __name__ == "__main__":
+    viz = Visualizer(params)
     params.best = 0.0
     params.n_labels = 10
-    params.labelled, params.unlabelled, params.validation = get_mnist(params,location="./", batch_size=100, labels_per_class=100)
+    # params.labelled, params.unlabelled, params.validation = get_mnist(params,location="./", batch_size=100, labels_per_class=100)
+    params = get_dataset(params)
     params.alpha = 0.1 * len(params.unlabelled) / len(params.labelled)
     params.epochs = 251
     params.step = 0
-    model = DeepGenerativeModel([784, 10, 50, [600, 600]])
+    model = DeepGenerativeModel([params.xdim, params.n_labels, 50, [600, 600]])
     if params.cuda:
         model = model.cuda()
     # , sampler=sampler) #,beta=beta)
--- utils/getData.py
+++ utils/getData.py
@@ -3,10 +3,13 @@ import numpy as np
 import sys
 from functools import reduce
 from operator import __or__
+from sklearn.preprocessing import MinMaxScaler
+from torch.utils import data
 from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision.datasets import MNIST
 import torchvision.transforms as transforms
 from utils import *
+from scipy import sparse
 def get_mnist(params, location="./", batch_size=64, labels_per_class=100):
 
 
@@ -37,3 +40,56 @@ def get_mnist(params, location="./", batch_size=64, labels_per_class=100):
                                              sampler=get_sampler(mnist_valid.test_labels.numpy()))
 
     return labelled, unlabelled, validation
+
+class Dataset(data.Dataset):
+    def __init__(self, params, dtype, scaler=None):
+        if scaler is None:
+            x_for_pp = np.load('datasets/' + params.data_set + '/x_tr.npy')
+            pp = MinMaxScaler()
+            self.scaler = pp.fit(x_for_pp)
+        else:
+            self.scaler = scaler
+        # self.x = self.scaler.transform(np.load('datasets/' + params.data_set + '/x_' + dtype + '.npy')).astype('float32')
+        self.x = np.load('datasets/' + params.data_set + '/x_' + dtype + '.npy').astype('float32')
+        self.y = np.load('datasets/' + params.data_set + '/y_' + dtype + '.npy').astype('float32')
+
+    def __len__(self):
+        return self.x.shape[0]
+
+    def getClasses(self):
+        return self.y.shape[-1]
+    def getDims(self):
+        return self.x.shape[-1]
+    def getScaler(self):
+        return self.scaler
+
+    def __getitem__(self, index):
+        # Select sample
+
+        # Load data and get label
+        X = torch.from_numpy(self.x[index, :].reshape((1, self.x.shape[-1])))#torch.load('data/' + ID + '.pt')
+        y = torch.from_numpy(self.y[index, :].reshape((1, self.y.shape[-1])))#self.labels[ID]
+        # import pdb
+        # pdb.set_trace()
+
+        return X, y
+
+def get_dataset(params):
+    if params.data_set=="mnist":
+        params.labelled, params.unlabelled, params.validation =  get_mnist(params)
+        params.n_labels = 10
+        params.xdim = 784
+    else:
+        args = {'batch_size': 100,
+            'shuffle': True,
+            'num_workers': 2}
+        params.labelled = Dataset(params, "subs")
+        params.n_labels = params.labelled.getClasses()
+        params.xdim = params.labelled.getDims()
+        scaler = params.labelled.getScaler()
+        
+        params.labelled = data.DataLoader(params.labelled, **args)
+        params.unlabelled = data.DataLoader(Dataset(params, "tr", scaler), **args)
+        params.validation = data.DataLoader(Dataset(params, "te", scaler), **args)
+    return params
+
