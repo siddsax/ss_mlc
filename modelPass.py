1bba8c2d93eae25fd8398b9a612394606c3088d8
siddsax <ss.siddharthasaxena@gmail.com>
2019-01-20 20:00:03 +0200

    working

--- modelPass.py
+++ modelPass.py
@@ -6,7 +6,6 @@ from operator import __or__
 from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision.datasets import MNIST
 import torchvision.transforms as transforms
-# sys.path.append("../semi-supervised")
 from utils import *
 from layers import *
 from models import DeepGenerativeModel
@@ -23,8 +22,7 @@ def modelTrPass(model, optimizer, elbo, params, logFile, epoch, viz=None):
   for iterator, ((u, _), (x, y)) in enumerate(params.allData):
 
       params.kl_annealling = 1 - 1.0 * np.exp(- params.step*params.factor*1e-5)
-
-      params.temperature = max(.5, 1.0*np.exp(-params.step*3e-4)) #default
+      params.temperature = max(.5, 1.0*np.exp(-params.step*3e-4))
 
       x, y, u = Variable(x).squeeze().float(), Variable(y).squeeze().float(), Variable(u).squeeze().float()
       if params.cuda:
@@ -35,14 +33,13 @@ def modelTrPass(model, optimizer, elbo, params, logFile, epoch, viz=None):
       # classication_loss = - torch.sum(y * torch.log(logits + 1e-8), dim=1).mean()
       classication_loss = params.alpha * torch.nn.functional.binary_cross_entropy(preds, y)*y.shape[-1]
 
-
       if params.ss:
-        L, kl, recon, prior = elbo(x, y=y)
-        U, klU, reconU, H, priorU = elbo(u, temperature=params.temperature, normal=params.normal)
-        loss = L + classication_loss + U
+          L, kl, recon, prior = elbo(x, y=y)
+          U, klU, reconU, H, priorU = elbo(u, temperature=params.temperature, normal=params.normal)
+          loss = L + classication_loss + U
       else:
-        kl, klU, recon, reconU, H, prior, priorU  = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-        loss = classication_loss
+          kl, klU, recon, reconU, H, prior, priorU  = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+          loss = classication_loss
 
       total_loss = loss.data.cpu().numpy()
       loss.backward()
@@ -53,7 +50,7 @@ def modelTrPass(model, optimizer, elbo, params, logFile, epoch, viz=None):
       params.step += 1
 
       if(iterator % int(max(m/6, 5))==0):
-      # if((iterator % 12)==0):
+
         toPrint = "[TRAIN]:({}, {}/{});Total {:.2f}; KL_label {:.2f}, Recon_label {:.2f}; KL_ulabel {:.2f}, Recon_ulabel {:.2f}, entropy {:.2f}; Classify_loss {:.2f}; prior {:.2f}; priorU {:.2f}".format(
           float(params.epoch), float(iterator), float(m), float(total_loss), float(kl), float(recon), float(klU), float(reconU), float(H), float(classication_loss), float(prior), float(priorU)
         )
@@ -64,18 +61,17 @@ def modelTrPass(model, optimizer, elbo, params, logFile, epoch, viz=None):
 
   P = 100*precision_k(y.data.cpu().numpy().squeeze(),preds.data.cpu().numpy().squeeze(), 5)
   if params.ss:
-    return [P[0], mseLoss, 100*params.temperature, recon], ['Prec_1', 'BCELoss', 'temperatureeraturex100', 'lblLossTrain']
+     return [P[0], mseLoss, 100*params.temperature, recon], ['Prec_1', 'BCELoss', 'temperatureeraturex100', 'lblLossTrain']
   else:
-    return [P[0], mseLoss], ['Prec_1', 'BCELoss']
+     return [P[0], mseLoss], ['Prec_1', 'BCELoss']
 
 def modelTePass(model, elbo, params, optimizer, logFile, testBatch=5000):
+
   model.eval()
   mseLoss, total_loss, labelled_loss, unlabelled_loss, kl, recon, Lpred, Lgt, reconU  = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
   m = len(params.validation)
-  ypred = []
-  ygt = []
-  XAll = []
-  dataPts = 0
+  ypred, ygt, XAll, dataPts = [], [], [], 0
+
   for x, y in params.validation:
       x, y = Variable(x).squeeze().float(), Variable(y).squeeze().float()
       dataPts +=x.shape[0]
@@ -88,14 +84,13 @@ def modelTePass(model, elbo, params, optimizer, logFile, testBatch=5000):
       L, klA, reconA, prior = elbo(x, y=y)
       logits, preds = model.classify(x)
 
-      # classication_loss = -torch.sum(y * torch.log(logits + 1e-8), dim=1).mean()
       classication_loss = torch.nn.functional.binary_cross_entropy(preds, y)*y.shape[-1]
       loss = L + params.alpha * classication_loss + U
 
       total_loss += loss.data.cpu().numpy()
       labelled_loss += L.data.cpu().numpy()
       unlabelled_loss += U.data.cpu().numpy()
-      mseLoss += classication_loss.data.cpu().numpy()#torch.mean((pred_idx.data == lab_idx.data).float())
+      mseLoss += classication_loss.data.cpu().numpy()
       kl += klA
       recon += reconA
       reconU += reconAU
@@ -109,31 +104,20 @@ def modelTePass(model, elbo, params, optimizer, logFile, testBatch=5000):
   ygt, ypred, XAll = np.concatenate(ygt, axis=0), np.concatenate(ypred, axis=0), np.concatenate(XAll, axis=0)
   P = 100*precision_k(ygt, ypred,5)
   if P[0] > params.bestP:
-    params.bestP = P[0]
-  # save_model(model, optimizer, params.epoch, params, "/model_best_test_" + params.mn + "_" + str(params.ss))
-  # if mseLoss / m < params.best:
-  #   params.best = mseLoss / m
-  
-  # toPrint = "[TEST]:temperature {:.3f}, Factor {:.3f}, Total Loss {:.2f}, Labelled Loss {:.2f}, KL {:.2f}, recon {:.2f}, unlabelled loss {:.2f}, mseLoss {:.2f}, best_p1 {}, best_bce {:.2f}".format(
-  #       float(params.temperature), params.reconFact.data.cpu().numpy(), float(total_loss / m), float(labelled_loss/ m), float(kl/m), float(recon/m), float(unlabelled_loss/ m), float(mseLoss/ m), params.bestP, params.best)
+      params.bestP = P[0]
+
   toPrint = 'recon {:.2f}, reconU {:.2f} lblLossPred {:.2f}, lblLossGT {:.2f}'.format(float(recon/m), float(reconU/m), Lpred / m, Lgt/m)
   toPrint += " || Prec Best " + str(params.bestP) + " Prec. " + str(P[0])+ " " + str(P[2]) + " " + str(P[4])
-  print("-"*20)
-  # Test the model
-  preds = model.predict_threshold(XAll, ypred)
 
-  f1_macro = f1_measure(ygt, preds, average='macro')
-  f1_micro = f1_measure(ygt, preds, average='micro')
-
-  toPrint += "f1_macro {:.2f} f1_micro {:.2f}".format(100*f1_macro, 100*f1_micro)
   logFile.write(toPrint + '\n')
   logFile.flush()
   print(toPrint)
   optimizer.zero_grad()
   model.train()
+
   if params.ss:
         return [P[0], mseLoss / m, Lpred / m, Lgt/m], ['Prec_1_Test', 'BCELossTest', 'lblLossPred', 'lblLossGT']
   else:
         return [P[0], mseLoss / m], ['Prec_1_Test', 'BCELossTest']
-  # return [P[0], mseLoss / m], ['Prec_1_Test', 'BCELossTest']
+
 
