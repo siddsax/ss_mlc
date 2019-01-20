b366f7078443cb987123798e3d8c2273fbabb3e9
siddhartha <ss.siddharthasaxena@gmail.com>
2019-01-20 19:30:18 +0530

    2

--- inference/variational.py
+++ inference/variational.py
@@ -87,7 +87,7 @@ class SVI(nn.Module):
                 xs = xs.repeat(self.model.y_dim, 1)
             else:
                 if temperature is None:
-                    print("Error, temperatureerature not given: Exiting")
+                    print("Error, temperature not given: Exiting")
                     exit()
                 # ys = gumbel_softmax(preds, temperature)
                 ys = gumbel_multiSample(logits, temperature)
@@ -95,13 +95,11 @@ class SVI(nn.Module):
         reconstruction = self.model(xs, ys)
 
         # p(x|y,z)
-        # likelihood = -self.likelihood(reconstruction, xs)
         diff = reconstruction - xs
         likelihood = - torch.sum(torch.mul(diff, diff), dim=-1)
         # p(y)
         prior = -log_standard_categorical(ys)
 
-        # L = (1 - self.params.kl_annealling) * likelihood - next(self.beta) * self.model.kl_divergence + prior
         L = likelihood + prior- float(self.params.kl_annealling) * self.model.kl_divergence
         if is_labelled:
             return - torch.mean(L) , np.mean(self.model.kl_divergence.data.cpu().numpy()), - np.mean(likelihood.data.cpu().numpy()), - np.mean(prior.data.cpu().numpy())
@@ -111,7 +109,6 @@ class SVI(nn.Module):
             L = torch.sum(torch.mul(logits, L), dim=-1)
 
         # Calculate entropy H(q(y|x)) and sum over all labels
-        # H = -torch.sum(torch.mul(preds, torch.log(preds + 1e-8)), dim=-1)
         H = - (torch.sum(torch.mul(preds, torch.log(preds + 1e-8)) + torch.mul(1 - preds, torch.log(1 - preds + 1e-8)), dim=-1))
 
         # Carefully written
--- utils/sample_data.py
+++ utils/sample_data.py
@@ -1,62 +1,40 @@
 import numpy as np
 from scipy import sparse
 import time
+import random
+import argparse
+
+params = argparse.ArgumentParser(description='Process some integers.')
+params.add_argument('--p', dest='percentage', required=True, type=float, help='percentage to sample data')
+params.add_argument('--d', dest='dataset', required=True, type=str, help='directory of dataset')
+params = params.parse_args()
+
 try:
-	x  = np.load('x_tr.npy')
-	ty = np.load('y_tr.npy')
+	x  = np.load(params.dataset + '/x_tr.npy')
+	ty = np.load(params.dataset + '/y_tr.npy')
 	sprse = 0
 except:
-	x  = sparse.load_npz('x_tr.npz')#.todense()
-	ty = sparse.load_npz('y_tr.npz')#.todense()
+	x  = sparse.load_npz(params.dataset + '/x_tr.npz')
+	ty = sparse.load_npz(params.dataset + '/y_tr.npz')
 	sprse = 1
-ty_cp = ty.copy() 
-ty = ty_cp.copy()
-labels_num = np.sum(ty,axis=0)
-removed_indices = []
-print(ty.shape)
+
+# Shuffling
+index_shuf = range(x.shape[0])
+random.shuffle(index_shuf)
+
+x = x[index_shuf]
+ty = ty[index_shuf]
+
 N = ty.shape[0]
-nf = .05*N
-n_now = N
-labels_num = np.sum(ty,axis=0)
-not_zero = np.argwhere(labels_num!=0)
-def index_rows_by_exclusion_nptake(arr, i):
-    """
-    Return copy of arr excluding single row of position i using
-    numpy.take function
-    """
-    return arr.take(range(i)+range(i+1,arr.shape[0]), axis=0)
+nf = int(params.percentage*N/100.0)
+x = x[:nf]
+ty = ty[:nf]
 
-# indices = np.random.randint(0, high=N, size=N)
-trigger = 0
-while (ty.shape[0] > nf):
+print(x.shape, ty.shape)
 
-    candidate = labels_num - ty[0]
-    not_zero = np.argwhere(labels_num!=0)[0,:]
-    #print(not_zero)
-    if(len(np.argwhere(candidate[not_zero]==0))==0):
-        print("="*10 + " "*5 + str(ty.shape[0]) + "/" + str(nf) +" "*5 + "="*10)
-	labels_num = candidate
-    	ty = ty[1:]
-	x = x[1:]
-	print('-'*10)
-	trigger = 0
-    else:
-	trigger +=1
-	if sprse:
-		ty = sparse.vstack((ty[1:], ty[0]))
-                x = sparse.vstack((x[1:], x[0]))
-	else:
-		ty = np.concatenate((ty[1:], ty[0].reshape((1, ty[0].shape[0]))), axis=0)
-		x = np.concatenate((x[1:], x[0].reshape((1, x[0].shape[0]))), axis=0)
-	print("Num labels offended: " +str(len(np.argwhere(candidate[not_zero]==0))))
-    if trigger > ty.shape[0]/10:
-	break
-    #break
-print(x.shape)
-print(ty.shape)
 if sprse:
-	sparse.save_npz('x_subs', x)
-	sparse.save_npz('y_subs', ty)
+	sparse.save_npz(params.dataset + 'x_subs', x)
+	sparse.save_npz(params.dataset + 'y_subs', ty)
 else:
-	np.save('x_subs', x)
-	np.save('y_subs', ty)
+	np.save(params.dataset + '/x_subs', x)
+	np.save(params.dataset + '/y_subs', ty)
